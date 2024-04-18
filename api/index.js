b326daf292fbcfc19ff56af8caad9553c4aa3c54
riishabhraj <rishabhraj5421@gmail.com>
2024-04-18 13:58:34 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
-app.listen(3000,()=> {
-    console.log("hey")
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/real-estate")
+  //   .connect
+  // "mongodb+srv://admin:admin@cluster0.3dhqvsx.mongodb.net/real-estate-backend"
+  //   ()
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("hey");
+});
