ad6dd15c81edf83456248b3d00e84634e5557e4c
riishabhraj <rishabhraj5421@gmail.com>
2024-05-03 20:48:06 +0530

    fixed user controller

--- api/controllers/user.contoller.js
+++ api/controllers/user.contoller.js
@@ -1,6 +1,7 @@
 import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcrypt";
 
 export const test = (req, res) => {
   res.json({
